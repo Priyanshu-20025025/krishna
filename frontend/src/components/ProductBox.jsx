69cdef82d801b041f55e0e5012bb1a3edd191179
Lunaredgepvtltd <info@lunaredge.in>
2024-09-12 17:12:01 +0530

    check commit

--- frontend/src/components/ProductBox.jsx
+++ frontend/src/components/ProductBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductBox = () => {
   return (
-    <div className='mx-auto border w-[18%] h-[500px] '>
+    <div className='mx-auto border w-[18%] h-[501px] '>
       <div className='w-full bg-[#2c4d27] text-white '>WHEAT</div>
     </div>
   )
